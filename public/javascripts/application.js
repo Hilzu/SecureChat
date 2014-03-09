2f7b6b8c3e60873cf625f293944a35db19a2253c
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2014-03-09 21:20:58 +0200

    Use Transparency for templating

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,18 +16,7 @@ app.loadPublicKey = function() {
 
 app.loadMessages = function() {
   $.getJSON('/messages', function(messages) {
-    var html = '<ul id="messagesList">';
-    $.each(messages, function(index, msg) {
-      html += '<li>';
-      html += '<span class="timestamp">' + msg.timestamp + '</span>';
-      html += '<span class="sender">' + msg.sender + '</span>';
-      html += ' -> ';
-      html += '<span class="receiver">' + msg.receiver + '</span>';
-      html += '<span class="message">' + msg.message + '</span>';
-      html += '</li>';
-    });
-    html += '</ul>';
-    $('#messages').html(html);
+    $('.messages').render(messages);
   });
 };
 
