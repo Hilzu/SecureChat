16c6a37fc71bfef886815eea043fbdafc75b7570
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2014-05-11 15:40:57 +0300

    Add nodemon and develop task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
 
 var paths = {
   projectScripts: ['./bin/www', '**/*.js', '!node_modules/**', '!public/**']
 };
 
 gulp.task('default', ['lint', 'watch'], function() {
+});
+
+gulp.task('develop', function () {
+  nodemon({ script: 'bin/www', ext: 'js' })
+    .on('change', ['lint'])
+    .on('restart', function () {
+      console.log('Restared!');
+    });
 });
 
 gulp.task('lint', function () {
